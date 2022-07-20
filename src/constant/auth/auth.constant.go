881533180c79c7cf11c76f72c3afde45c826ea15
samithiwat <samithiwat2544@gmail.com>
2022-07-21 01:21:03 +0700

    Exclude baan from auth guard

--- src/constant/auth/auth.constant.go
+++ src/constant/auth/auth.constant.go
@@ -3,6 +3,8 @@ package auth
 var ExcludePath = map[string]struct{}{
 	"POST /auth/verify":       {},
 	"POST /auth/refreshToken": {},
+	"GET /baan":               {},
+	"GET /baan/:id":           {},
 }
 
 var MapPath2Phase = map[string][]string{
@@ -14,6 +16,8 @@ var MapPath2Phase = map[string][]string{
 	"POST /group/:token":        {"select"},
 	"DELETE /group/leave":       {"select"},
 	"PUT /group":                {"select"},
+	"GET /baan":                 {"select"},
+	"GET /baan/:id":             {"select"},
 	"DELETE /group/members/:id": {"select"},
 	"POST /qr/checkin/verify":   {"eventDay", "eStamp"},
 	"POST /qr/checkin/confirm":  {"eventDay", "eStamp"},
