fda81f53c37b1e331446382fc13f4e07971cb6ec
ImSoZRious <pannawichtata@gmail.com>
2023-07-08 23:19:40 +0700

    chore: update doc

--- src/dto/auth.rs
+++ src/dto/auth.rs
@@ -2,7 +2,9 @@ use utoipa::ToSchema;
 
 #[derive(serde::Deserialize, ToSchema)]
 pub struct VerifyTicket {
-    #[schema(example = "AAAAAAAAAAo2NTQwMDAwMDIxAAAAAAAAAARKb2huAAAAAAAAAANEb2UAAAAAAAAAFAu8DXGVMGlZFIP0MlQSHIOkOxLE")]
+    #[schema(
+        example = "AAAAAAAAAAo2NTQwMDAwMDIxAAAAAAAAAARKb2huAAAAAAAAAANEb2UAAAAAAAAAFAu8DXGVMGlZFIP0MlQSHIOkOxLE"
+    )]
     pub ticket: String,
 }
 
@@ -21,3 +23,20 @@ pub struct RedeemNewToken {
 pub struct Validate {
     pub token: String,
 }
+
+#[derive(serde::Deserialize, serde::Serialize, ToSchema)]
+pub struct Credential {
+    #[schema(example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3BiZX...")]
+    pub access_token: String,
+    pub refresh_token: String,
+    #[schema(example = 3600)]
+    pub expires_in: i32,
+}
+
+into_dto!(
+    rpkm66_rust_proto::rpkm66::auth::auth::v1::Credential,
+    Credential,
+    access_token,
+    refresh_token,
+    expires_in
+);
