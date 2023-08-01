1adbe0f0da53f16a408e99e9f3a42aa67589a516
ImSoZRious <pannawichtata@gmail.com>
2023-08-01 19:42:44 +0700

    handle error

--- src/dto/checkin.rs
+++ src/dto/checkin.rs
@@ -39,5 +39,3 @@ impl IntoDto for CheckinResponse {
         self
     }
 }
-
-
--- src/service/checkin.rs
+++ src/service/checkin.rs
@@ -2,7 +2,7 @@ use crate::Result;
 use rpkm66_rust_proto::rpkm66::checkin::user::v1::{
     user_service_client::UserServiceClient, AddEventRequest, GetUserEventByEventIdRequest,
 };
-use tonic::transport::Channel;
+use tonic::{transport::Channel, Code};
 
 #[derive(Clone)]
 pub struct Service {
@@ -16,17 +16,20 @@ impl Service {
     }
 
     pub async fn has_checkin(&self, user_id: String) -> Result<bool> {
-        Ok(self
+        let req = self
             .client
             .clone()
             .get_user_event_by_event_id(GetUserEventByEventIdRequest {
-                user_id,
                 event_id: self.get_checkin_event_id(),
+                user_id,
             })
-            .await?
-            .into_inner()
-            .user_event
-            .is_some())
+            .await;
+
+        match req {
+            Ok(x) => Ok(x.into_inner().user_event.is_some()),
+            Err(e) if e.code() == Code::NotFound => Ok(false),
+            Err(e) => Err(e.into()),
+        }
     }
 
     pub async fn checkin(&self, user_id: String) -> Result<bool> {
